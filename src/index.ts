fe5b32eda1edcd44a3b187cedf10fe9e1a7b3043
DiilaNa <liyanaarachchidilan@gmail.com>
2025-12-15 14:58:17 +0530

    Update cors in index

--- src/index.ts
+++ src/index.ts
@@ -1,44 +1,47 @@
-import  express from "express";
+import express from "express";
 import mongoose from "mongoose";
-import { log , error } from "console";
-import dotenv from "dotenv"
-import cors from "cors"
-import auth from "./routes/auth.routes"
+import { log, error } from "console";
+import dotenv from "dotenv";
+import cors from "cors";
+import auth from "./routes/auth.routes";
 import announcementsRouter from "./routes/announcements.routes";
 import categoriesRouter from "./routes/category.routes";
 import brandsRouter from "./routes/brands.routes";
 import warrantyRouter from "./routes/warranties.routes";
 import notificationRouter from "./routes/notification.routes";
 
-dotenv.config()
+dotenv.config();
 
 const SERVER_PORT = process.env.SERVER_PORT;
-const MONGO_URI = process.env.MONGO_URI as string
+const MONGO_URI = process.env.MONGO_URI as string;
 
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
-app.use(cors({
-  origin:["http://localhost:5173"],
-  methods:["GET","POST","PUT","DELETE","PATCH","OPTIONS"]
-}))
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+  })
+);
 
-app.use("/api/v1/auth",auth)
-app.use("/api/v1/announcements",announcementsRouter)
-app.use("/api/v1/categories", categoriesRouter)
-app.use("/api/v1/brands", brandsRouter)
-app.use("/api/v1/warranties",warrantyRouter)
-app.use("/api/v1/notifications", notificationRouter)
+app.use("/api/v1/auth", auth);
+app.use("/api/v1/announcements", announcementsRouter);
+app.use("/api/v1/categories", categoriesRouter);
+app.use("/api/v1/brands", brandsRouter);
+app.use("/api/v1/warranties", warrantyRouter);
+app.use("/api/v1/notifications", notificationRouter);
 
 mongoose
-.connect(MONGO_URI)
-.then(()=>{
-    log("DB Connected")
-}).catch((err)=>{
-    error(err)
-    process.exit(1)
-})
+  .connect(MONGO_URI)
+  .then(() => {
+    log("DB Connected");
+  })
+  .catch((err) => {
+    error(err);
+    process.exit(1);
+  });
 
 app.listen(SERVER_PORT, () => {
   console.log(`Server running on port ${SERVER_PORT}`);

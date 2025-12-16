795816534c8ae91b214ae090b5722fd6d5450f4e
DiilaNa <liyanaarachchidilan@gmail.com>
2025-12-16 11:47:06 +0530

    updated URLs

--- src/index.ts
+++ src/index.ts
@@ -22,7 +22,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["https://keepify-the-product-warranty-tracke.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
   })
 );
